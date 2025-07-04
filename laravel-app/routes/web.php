be5ef783f4f0c77d4a60c07da9c312f14a91b544
Dwiki Yulian <79854897+qjvxvL@users.noreply.github.com>
2025-07-04 12:26:07 +0700

    Revert "signup & login front-end"
    This reverts commit 97f17320e65e0b69697514d116f90efdfa2b7a0b.

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -5,7 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+<<<<<<< HEAD
 
 Route::get('/login', function () {
     return view('login');
 });
+=======
+>>>>>>> parent of 4cfb469 (Merge branch 'main' of https://github.com/naufalokto/Mifta-Motorsport)
