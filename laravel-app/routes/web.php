152ed489f5baa4c3291c4a12a3a3736eb435f084
Dwiki Yulian <79854897+qjvxvL@users.noreply.github.com>
2025-07-04 12:38:46 +0700

    Revert "signup & login front-end"
    This reverts commit 97f17320e65e0b69697514d116f90efdfa2b7a0b.

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -11,6 +11,9 @@ Route::get('/login', function () {
     return view('login');
 <<<<<<< HEAD
 });
+<<<<<<< HEAD
 =======
 });
+>>>>>>> parent of 97f1732 (signup & login front-end)
+=======
 >>>>>>> parent of 97f1732 (signup & login front-end)
