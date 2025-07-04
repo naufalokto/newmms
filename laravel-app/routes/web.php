d8972dc5859ef45e1c97934f7b109cdb6a047269
farrelmalvin@gmail.com <farrelmalvin@gmail.com>
2025-07-04 12:23:30 +0700

    Implement authentication for customer & admin login, add role-based access middleware, update model structure, migrations, and seeders to support a secure and structured login system

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\CustomerDashboardController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,8 +10,4 @@ Route::get('/', function () {
 
 Route::get('/login', function () {
     return view('login');
-});
-
-Route::get('/signup', function () {
-    return view('signup');
 });
