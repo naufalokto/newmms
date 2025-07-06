ae58d03cb1237dd11f20939d13952fcc99f82a53
macbook <macbook@MacBook-Pro-macbook.local>
2025-07-06 20:18:25 +0700

    nyiapno dahsboard cust

---
+++ app/Models/Pengguna.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Pengguna extends Authenticatable
+{
+    protected $table = 'pengguna';
+    protected $primaryKey = 'id_pengguna';
+    public $timestamps = false;
+    protected $fillable = [
+        'nama', 'username', 'password', 'peran'
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+} 
