69c70ed5fa2ccd237a5d89f1c3489e85e11d023a
Dwiki Yulian <79854897+qjvxvL@users.noreply.github.com>
2025-07-21 17:53:49 +0700

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,40 @@ class DatabaseSeeder extends Seeder
             ServiceSeeder::class, // Tambahkan ServiceSeeder
             TestimoniSeeder::class, // Tambahkan TestimoniSeeder
             ProdukSeeder::class, // Tambahkan ProdukSeeder
+
+        ]);
+        // Tambahkan dummy produk secara manual
+        \App\Models\Produk::create([
+            'nama_produk' => 'Motul Oil',
+            'kategori' => 'Oil',
+            'harga' => 350000,
+            'stok' => 10,
+            'gambar_produk' => null,
+            'deskripsi' => 'High quality engine oil.'
+        ]);
+        \App\Models\Produk::create([
+            'nama_produk' => 'Ohlins Suspension',
+            'kategori' => 'Second Part',
+            'harga' => 28900000,
+            'stok' => 5,
+            'gambar_produk' => null,
+            'deskripsi' => 'Premium suspension for motorcycles.'
+        ]);
+        \App\Models\Produk::create([
+            'nama_produk' => 'Kawasaki H2R',
+            'kategori' => 'New Part',
+            'harga' => 760000000,
+            'stok' => 2,
+            'gambar_produk' => null,
+            'deskripsi' => 'Superbike for racing.'
+        ]);
+        \App\Models\Produk::create([
+            'nama_produk' => 'MMS T-Shirt',
+            'kategori' => 'Apparel',
+            'harga' => 150000,
+            'stok' => 20,
+            'gambar_produk' => null,
+            'deskripsi' => 'Official MMS apparel.'
         ]);
     }
 } 
